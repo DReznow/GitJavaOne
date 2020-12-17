395f2919d3e4f540b2aa9a3a5ac0de89e9c3a381
DawidReznow <dawid.reznow@awacademyconsultant.de>
2020-12-17 10:48:05 +0100

    new line code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello its my First git java project");
+        System.out.println("Hello its my First git java project");
+        System.out.println("-------------------------------------------");
+        System.out.println("blah blah blah new project for git");
+        System.out.println("-------------------------------------------");
+        System.out.println("another change... awesome another change");
     }
 }
