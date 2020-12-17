c218fa1c321a8dfd4db58c795d65084fd901cab6
DawidReznow <dawid.reznow@awacademyconsultant.de>
2020-12-17 11:01:05 +0100

    another class aded

---
+++ src/com/company/Person.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Person {
+    String name;
+    int age;
+    String city;
+
+    void fullInfo() {
+        System.out.printf("%s from %s is %d years old\n", name, city, age);
+    }
+}
+
